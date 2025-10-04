0577c496224595e1e2e8d98d46a6c93f8cd2444f
Apples <2352020+apples@users.noreply.github.com>
2025-10-04 12:42:35 -0500

    Random gen

--- scenes/main_gameplay/main_gameplay.gd
+++ scenes/main_gameplay/main_gameplay.gd
@@ -1,1 +1,16 @@
 extends Node3D
+
+const chunk_size = Vector3(2*16, 0, 2*16)
+const world_size = Vector2(10, 10)
+
+var default_chunks = [
+	preload("uid://di8uyh08twmfu"),
+]
+
+func _ready() -> void:
+	for i in world_size.x:
+		for j in world_size.y:
+			var chunk = default_chunks.pick_random().instantiate()
+			chunk.name = "Chunk_%s_%s" % [i, j]
+			chunk.position = Vector3(i - 5, 0, -j + 5) * chunk_size
+			add_child(chunk)
