3c9939e15e1eefde8e39db01da618f55328eb558
Jacob <jacob.wallace@utexas.edu>
2025-10-04 14:14:16 -0500

    goblins drop hats on death

--- actors/enemies/goblin/goblin.gd
+++ actors/enemies/goblin/goblin.gd
@@ -1,5 +1,7 @@
 extends base_enemy
 
+var hat_drop_scene = preload("uid://2ycmb5n45u20")
+
 func _init() -> void:
 	health = 3
 	damage = 1
@@ -8,7 +10,16 @@ func _init() -> void:
 
 func on_hit(_damage: int):
 	health -= _damage
-	print("ouch! you hit me")
 	
 	if health <= 0:
-		print("Gobbo died!")
+		on_death()
+
+func on_death():
+	spawn_hat_drop()
+	self.queue_free()
+
+func spawn_hat_drop():
+	var new_hat_drop = hat_drop_scene.instantiate()
+	var hat_drop_pos = Vector3(self.global_position.x, 0, self.global_position.z)
+	new_hat_drop.set_position(hat_drop_pos)
+	self.get_parent().add_child(new_hat_drop)
