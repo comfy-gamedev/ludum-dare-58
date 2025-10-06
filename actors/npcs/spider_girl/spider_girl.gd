bf4c5635f7e744125ae4b7e131075ee756489ca5
Jacob <jacob.wallace@utexas.edu>
2025-10-06 15:44:16 -0500

    zooming on spider girl works better

--- actors/npcs/spider_girl/spider_girl.gd
+++ actors/npcs/spider_girl/spider_girl.gd
@@ -3,14 +3,14 @@ extends Area3D
 var player_nearby = false
 var player_interacting = false
 var player_ref
-var camera_origin_position_vec
+var camera_original_rotation_vec
 	
 func _process(delta: float) -> void:
 	if player_nearby and Input.is_action_just_pressed("shoot"):
 		if not player_interacting:
 			on_player_interact(delta)
 
-func on_player_interact(delta: float):
+func on_player_interact(_delta: float):
 	player_interacting = true
 	on_zoom_camera()
 	print("interact")
@@ -18,13 +18,15 @@ func on_player_interact(delta: float):
 func on_zoom_camera():
 	if is_instance_valid(player_ref):
 		var camera_node = player_ref.get_node("Camera3D")
-		camera_origin_position_vec = camera_node.position
-		camera_node.position = Vector3(0, 20, 10)
+		camera_original_rotation_vec = camera_node.rotation
+		camera_node.global_position = Vector3(23, 1, 7)
+		camera_node.rotation = Vector3(25, 0, 0)
 
 func on_reset_camera_position():
 	if is_instance_valid(player_ref):
 		var camera_node = player_ref.get_node("Camera3D")
-		camera_node.position = camera_origin_position_vec
+		camera_node.global_position = player_ref.position + Vector3(-0.01, 8.322, 5.688) #camera_original_position_vec
+		camera_node.rotation = camera_original_rotation_vec
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
