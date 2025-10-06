6aadefe777d058d2ead4a7dd8e20e3da2bae13ff
HappyTrollAngus <206193088+HappyTrollAngus@users.noreply.github.com>
2025-10-05 22:31:29 -0500

    lotsa stuff

--- actors/hats/fez/fez.gd
+++ actors/hats/fez/fez.gd
@@ -1,1 +1,15 @@
-extends RigidBody3D
+extends Hat
+
+
+func _init() -> void:
+	element = Globals.elements.FIRE
+	use_cooldown = 0.8
+
+func fire(dir: Vector3, bullet_parent: Node3D):
+	var bullet: base_bullet = bullet_scene.instantiate()
+	bullet.team = self.team
+	bullet.position = global_position + (Vector3.DOWN * 2)
+	bullet.direction = dir
+	bullet.speed *= 2
+	bullet.element_type = Globals.elements.FIRE
+	bullet_parent.add_child(bullet)
