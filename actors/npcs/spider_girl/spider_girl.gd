23f594d355605ba18bcc0795610da8c069ff3139
Jacob <jacob.wallace@utexas.edu>
2025-10-06 14:43:19 -0500

    tuning player interact process

--- actors/npcs/spider_girl/spider_girl.gd
+++ actors/npcs/spider_girl/spider_girl.gd
@@ -1,12 +1,22 @@
 extends Area3D
 
+var player_nearby = false
+var player_interacting = false
+	
+func _process(_delta: float) -> void:
+	if player_nearby and Input.is_action_just_pressed("shoot"):
+		if not player_interacting:
+			on_player_interact()
 
-
-#func _ready() -> void:
-	#self.process_mode = Node.PROCESS_MODE_DISABLED
-
+func on_player_interact():
+	player_interacting = true
+	print("interact")
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		print("player <-> npc")
-		#if Input.is_action_pressed("shoot") and 
+		player_nearby = true
+
+func _on_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		player_nearby = false
+		player_interacting = false
