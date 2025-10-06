9966b1c0a48ffb46b170407ad790ad0aa2b8acba
Apples <2352020+apples@users.noreply.github.com>
2025-10-06 14:05:20 -0500

    Optimizations

--- addons/chunker/chunker.gd
+++ addons/chunker/chunker.gd
@@ -4,6 +4,8 @@ class_name Chunker
 
 const OFFSETS = [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 1)]
 
+const DECALS = [4, 5, 6, 7, 8, 35]
+
 @export var chunk_size: Vector2 = Vector2(32.0, 32.0)
 
 @export_storage var chunk_scenes: Array[String] = []
@@ -12,6 +14,10 @@ const OFFSETS = [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 1)]
 var _loaded_chunks: Dictionary[Vector2i, Node3D] = {}
 
 var _cached_load: Dictionary[int, PackedScene] = {}
+
+var _meshlib_copy: MeshLibrary
+
+var _cells_to_bump: Array[Vector3i] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -50,7 +56,43 @@ func _reload_chunk(p: Vector2i) -> void:
 		var chunk_p = p + Chunker.OFFSETS[chunks[p][1]]
 		_loaded_chunks[p].position = Vector3(chunk_p.x * chunk_size.x, 0, -chunk_p.y * chunk_size.y)
 		_loaded_chunks[p].rotation.y = (chunks[p][1] / 4.0) * TAU
-		for c in _loaded_chunks[p].get_children():
-			if c is DirectionalLight3D:
-				c.free()
+		_cleanup_chunk(_loaded_chunks[p], chunks[p][1])
 		add_child(_loaded_chunks[p], false, Node.INTERNAL_MODE_BACK)
+
+func _cleanup_chunk(node: GridMap, spin: int) -> void:
+	if not node:
+		return
+	
+	if not _meshlib_copy:
+		_meshlib_copy = node.mesh_library.duplicate()
+		for i in _meshlib_copy.get_item_list():
+			var j = i + 1000
+			_meshlib_copy.create_item(j)
+			_meshlib_copy.set_item_mesh(j, _meshlib_copy.get_item_mesh(i))
+	
+	node.cell_octant_size = 16
+	node.mesh_library = _meshlib_copy
+	
+	for v in node.get_used_cells():
+		var c = node.get_cell_item(v)
+		if not _meshlib_copy.get_item_shapes(c):
+			continue
+		if v.y < 0:
+			_cells_to_bump.append(v)
+		else:
+			var ii = 0
+			for vv in [Vector3i(1, 0, 0), Vector3i(-1, 0, 0), Vector3i(0, 0, -1), Vector3i(0, 0, 1)]:
+				if node.get_cell_item(v + vv) == c:
+					ii += 1
+			if ii == 4:
+				if c == 2 or node.get_cell_item(v + Vector3i(0, 1, 0)) == c:
+					_cells_to_bump.append(v)
+		#if c in DECALS:
+			#o = node.get_orthogonal_index_from_basis(Basis(Vector3.UP, ((3 - spin) / 4.0) * TAU))
+	
+	for v in _cells_to_bump:
+		var c = node.get_cell_item(v)
+		var o = node.get_cell_item_orientation(v)
+		node.set_cell_item(v, c + 1000, o)
+	
+	_cells_to_bump.clear()
