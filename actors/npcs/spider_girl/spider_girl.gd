9f4a258b6307d1daa7cc20b88dae77ad8cd7ecf1
Jacob <jacob.wallace@utexas.edu>
2025-10-06 18:35:39 -0500

    hard coding collection log but it works

--- actors/npcs/spider_girl/spider_girl.gd
+++ actors/npcs/spider_girl/spider_girl.gd
@@ -18,8 +18,26 @@ var yarn: int = 0
 @onready var marker_3d: Marker3D = $Marker3D
 @onready var marker_3d_2: Marker3D = $Marker3D2
 
-func _ready() -> void:
-	init_hat_catalog_items()
+var collected_hats = {
+	"res://actors/hats/beefeater/beefeater.tscn" = false,
+	"res://actors/hats/beret/beret.tscn" = false,
+	"res://actors/hats/bicorn/bicorn.tscn" = false,
+	"res://actors/hats/buffalo/buffalo.tscn" = false,
+	"res://actors/hats/cowboy/cowboy.tscn" = false,
+	"res://actors/hats/fez/fez.tscn" = false,
+	"res://actors/hats/jester/jester.tscn" = false,
+	"res://actors/hats/madder/madder.tscn" = false,
+	"res://actors/hats/mortarboard/mortarboard.tscn" = false,
+	"res://actors/hats/phrygian/phrygian.tscn" = false,
+	"res://actors/hats/scally/scally.tscn" = false,
+	"res://actors/hats/sombrero/sombrero.tscn" = false,
+	"res://actors/hats/tricorn/tricorn.tscn" = false,
+	"res://actors/hats/tyrolean/tyrolean.tscn" = false,
+	"res://actors/hats/witch/witch.tscn" = false,
+}
+
+#func _ready() -> void:
+	#init_hat_catalog_items()
 
 func _process(delta: float) -> void:
 	if player_nearby and Input.is_action_just_pressed("shoot"):
@@ -30,6 +48,10 @@ func on_player_interact(_delta: float):
 	player_interacting = true
 	on_zoom_camera()
 	intro_dialog()
+	
+	for child in catalog_panel.get_children():
+		child.queue_free()
+	init_hat_catalog_items()
 
 func on_zoom_camera():
 	if is_instance_valid(player_ref):
@@ -54,7 +76,15 @@ func _on_body_entered(body: Node3D) -> void:
 		
 		if not is_instance_valid(player_ref):
 			player_ref = body
+			
 	if body.is_in_group("hat") and not body.is_in_group("spiderhat"):
+		# Register newly collected hat.
+		if not collected_hats[body.scene_file_path]:
+			collected_hats[body.scene_file_path] = true
+		#print(Globals.hat_scene_pool["BERET"].resource_path)
+		#print(body.scene_file_path)
+		#print(Globals.hat_scene_pool.find_key(body.scene_file_path))
+		
 		body.queue_free()
 		yay.emitting = true
 		yarn += 1
@@ -144,37 +174,45 @@ func create_hat_catalog_item(pos: Vector2, hat_index):
 	#Globals.hat_scene_pool
 	catalog_panel.add_child(new_item_panel)
 	
-	var hat_keys = Globals.hat_scene_pool.keys()
+	var hat_keys = collected_hats.keys() # Globals.hat_scene_pool.keys()
 	if hat_index <= hat_keys.size():
-		var current_hat_key = hat_keys[hat_index]
-		var current_hat_scene = Globals.hat_scene_pool[current_hat_key]
-		var hat = current_hat_scene.instantiate()
-		
-		var sub_viewport_container = SubViewportContainer.new()
-		var sub_viewport = SubViewport.new()
-		sub_viewport.transparent_bg = true
-		#new_item_panel.add_child(sub_viewport_container)
-		sub_viewport_container.add_child(sub_viewport)
-		#sub_viewport.size = Vector2i(100, 100)
-		sub_viewport_container.stretch = true
-		sub_viewport_container.size = Vector2i(100, 100)
-		sub_viewport.add_child(Camera3D.new())
-		sub_viewport.add_child(hat)
-		sub_viewport.own_world_3d = true
-		var directional_light = DirectionalLight3D.new()
-		directional_light.light_energy = 0.75
-		directional_light.rotation = Vector3(-45, 45, 0)
-		sub_viewport.add_child(directional_light)
-		new_item_panel.add_child(sub_viewport_container)
-		hat.position.z = -2
-		hat.process_mode = Node.PROCESS_MODE_DISABLED
-		#new_item_panel.add_child(sub_viewport)
-		#sub_viewport.texture = 
-		#sub_viewport.add_child(hat)
-		#var viewport = new Viewport()
-		#hat.global_position = Vector3(pos.x, pos.y, 0)
-	#var hat_drop_pos = Vector3(self.global_position.x, 0, self.global_position.z)
-		#new_item_panel.add_child(hat)
+		#var current_hat_key = hat_keys[hat_index]
+		#var current_hat_scene = Globals.hat_scene_pool[current_hat_key]
+		var hat_scene_file = hat_keys[hat_index]
+		if collected_hats[hat_scene_file]:
+			var hat = load(hat_scene_file).instantiate()
+			#var hat = current_hat_scene.instantiate()
+			#var current_hat_key = hat_keys.find_key()
+			
+			var sub_viewport_container = SubViewportContainer.new()
+			var sub_viewport = SubViewport.new()
+			sub_viewport.transparent_bg = true
+			#new_item_panel.add_child(sub_viewport_container)
+			sub_viewport_container.add_child(sub_viewport)
+			#sub_viewport.size = Vector2i(100, 100)
+			sub_viewport_container.stretch = true
+			sub_viewport_container.size = Vector2i(100, 100)
+			sub_viewport.add_child(Camera3D.new())
+			sub_viewport.add_child(hat)
+			sub_viewport.own_world_3d = true
+			var directional_light = DirectionalLight3D.new()
+			directional_light.light_energy = 0.75
+			directional_light.rotation = Vector3(-45, 45, 0)
+			sub_viewport.add_child(directional_light)
+			new_item_panel.add_child(sub_viewport_container)
+			hat.position.z = -2
+			hat.process_mode = Node.PROCESS_MODE_DISABLED
+			
+			#new_item_panel.text = "?"
+			#new_item_panel.add_child(sub_viewport)
+			#sub_viewport.texture = 
+			#sub_viewport.add_child(hat)
+			#var viewport = new Viewport()
+			#hat.global_position = Vector3(pos.x, pos.y, 0)
+			#var hat_drop_pos = Vector3(self.global_position.x, 0, self.global_position.z)
+			#new_item_panel.add_child(hat)
+		else:
+			new_item_panel.text = "?"
 	else:
 		new_item_panel.text = "?"
 
