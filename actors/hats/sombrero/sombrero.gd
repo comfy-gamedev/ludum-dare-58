3504b39221262ff05187afd7e9939c92aaaaf30d
Jacob <jacob.wallace@utexas.edu>
2025-10-14 03:51:21 -0500

    fixed weird issue with beret getting reset to the sombrero script

--- actors/hats/sombrero/sombrero.gd
+++ actors/hats/sombrero/sombrero.gd
@@ -14,3 +14,33 @@ func fire(dir: Vector3, bullet_parent: Node3D):
 	bullet.size = 2
 	bullet.element_type = element
 	bullet_parent.add_child(bullet)
+
+
+### Potentially old sombrero code below.
+#extends Hat
+#func _init() -> void:
+	#element = Globals.elements.FIRE
+	#use_cooldown = 0.75
+	#
+#
+#func fire(dir: Vector3, bullet_parent: Node3D):
+	#var bullet: base_bullet = bullet_scene.instantiate()
+	#bullet.team = self.team
+	#bullet.position = global_position
+	#bullet.position.y = 1
+	#bullet.direction = dir
+	#
+	##this block is where you change the values
+	#bullet.element_type = element
+	#bullet.damage = 1
+	#bullet.speed = 8.5
+	#bullet.size = 1
+	#bullet.lifetime = 1.5
+	#bullet.movement = base_bullet.movement_types.STRAIGHT
+	#bullet.piercing = 1
+	#bullet.radius = 3.0 #only applied to orbital or pulse modes
+	#bullet.explosive = false
+	#bullet.slowing = false
+	#bullet.homing = false
+	#
+	#bullet_parent.add_child(bullet)
