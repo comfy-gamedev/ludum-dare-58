39b43c0a84331e2964e9b9392d069879b9bd514a
HappyTrollAngus <206193088+HappyTrollAngus@users.noreply.github.com>
2025-10-14 03:48:31 -0500

    Update beret.gd

--- actors/hats/beret/beret.gd
+++ actors/hats/beret/beret.gd
@@ -1,7 +1,7 @@
 extends Hat
 func _init() -> void:
 	element = Globals.elements.FIRE
-	use_cooldown = 1.5
+	use_cooldown = 5
 
 func fire(dir: Vector3, bullet_parent: Node3D):
 	var bullet: base_bullet = bullet_scene.instantiate()
