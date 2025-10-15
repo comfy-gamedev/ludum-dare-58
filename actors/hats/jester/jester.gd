2374ca2b8b0e27920dce1a3d53dccd446741efd1
Jacob <jacob.wallace@utexas.edu>
2025-10-14 21:57:05 -0500

    reverting jester orbital type hat back to normal for wired to fix

--- actors/hats/jester/jester.gd
+++ actors/hats/jester/jester.gd
@@ -5,13 +5,13 @@ func _init() -> void:
 	element = Globals.elements.EARTH
 	use_cooldown = 0.8
 
-func fire(dir: Vector3, bullet_parent: Node3D):
+func fire(dir: Vector3, _bullet_parent: Node3D):
 	var bullet: base_bullet = bullet_scene.instantiate()
 	bullet.team = self.team
-	bullet.position = global_position
-	bullet.position.y = 1
+	bullet.position = position + (Vector3.DOWN * 2)
+	#bullet.position.y = 1
 	bullet.direction = dir
 	bullet.speed *= 1.5
 	bullet.element_type = element
 	bullet.movement = base_bullet.movement_types.ORBITAL
-	bullet_parent.add_child(bullet)
+	add_child(bullet)
