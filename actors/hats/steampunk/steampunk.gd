017f3846f7b85d3615a7fad3800157a88e94ab50
HappyTrollAngus <206193088+HappyTrollAngus@users.noreply.github.com>
2025-10-18 22:45:29 -0500

    updates

--- actors/hats/steampunk/steampunk.gd
+++ actors/hats/steampunk/steampunk.gd
@@ -2,7 +2,7 @@ extends Hat
 
 func _init() -> void:
 	element = Globals.elements.FIRE
-	use_cooldown = 0.5
+	use_cooldown = 0.4
 
 func fire(dir: Vector3, bullet_parent: Node3D):
 	var bullet: base_bullet = bullet_scene.instantiate()
@@ -13,14 +13,14 @@ func fire(dir: Vector3, bullet_parent: Node3D):
 	#this block is where you change the values
 	bullet.element_type = Globals.elements.FIRE
 	bullet.damage = 1
-	bullet.speed = 7.5
-	bullet.size = 1
-	bullet.lifetime = 2
+	bullet.speed = 10
+	bullet.size = 0.5
+	bullet.lifetime = 1.5
 	bullet.movement = base_bullet.movement_types.STRAIGHT
 	bullet.piercing = 1
 	bullet.radius = 3.0 #only applied to orbital or pulse modes
 	bullet.explosive = false
 	bullet.slowing = true
-	bullet.homing = true
+	bullet.homing = false
 	
 	bullet_parent.add_child(bullet)
--- actors/hats/thecat/thecat.gd
+++ actors/hats/thecat/thecat.gd
@@ -2,25 +2,65 @@ extends Hat
 
 func _init() -> void:
 	element = Globals.elements.FIRE
-	use_cooldown = 1.0
+	use_cooldown = 5.0
 
 func fire(dir: Vector3, bullet_parent: Node3D):
 	var bullet: base_bullet = bullet_scene.instantiate()
 	bullet.team = self.team
-	bullet.position = global_position + (Vector3.DOWN * 2)
+	bullet.position = global_position
+	bullet.position.y = 1
 	bullet.direction = dir
+	#bullet.direction = dir.rotated(Vector3.UP, 0)
+	bullet.lifetime = 4
+	bullet.movement = base_bullet.movement_types.STRAIGHT
+	bullet.element_type = element
+	bullet_parent.add_child(bullet)
 	
-	#this block is where you change the values
-	bullet.element_type = Globals.elements.FIRE
-	bullet.damage = 1
-	bullet.speed = 8
-	bullet.size = 2
-	bullet.lifetime = 3
+	bullet = bullet_scene.instantiate()
+	bullet.team = self.team
+	bullet.position = global_position
+	bullet.position.y = 1
+	bullet.direction = dir.rotated(Vector3.UP, TAU / 3)
+	bullet.lifetime = 4
 	bullet.movement = base_bullet.movement_types.LOOPY
-	bullet.piercing = 1
-	bullet.radius = 3.0 #only applied to orbital or pulse modes
-	bullet.explosive = false
-	bullet.slowing = false
+	bullet.element_type = element
 	bullet.homing = true
+	bullet_parent.add_child(bullet)
+	#
+	bullet = bullet_scene.instantiate()
+	bullet.team = self.team
+	bullet.position = global_position
+	bullet.position.y = 1
+	bullet.direction = dir.rotated(Vector3.UP, (TAU * 2) / 3)
+	bullet.lifetime = 4
+	bullet.movement = base_bullet.movement_types.LOOPY
+	bullet.element_type = element
+	bullet.homing = true
+	bullet.explosive = true
+	bullet.radius = 2.0
+	bullet_parent.add_child(bullet)
 	
+	bullet = bullet_scene.instantiate()
+	bullet.team = self.team
+	bullet.position = global_position
+	bullet.position.y = 1
+	bullet.direction = dir.rotated(Vector3.UP, (TAU * 3) / 3)
+	bullet.lifetime = 4
+	bullet.movement = base_bullet.movement_types.LOOPY
+	bullet.element_type = element
+	bullet.homing = true
+	bullet.explosive = true
+	bullet.radius = 2.0
+	bullet_parent.add_child(bullet)
+	
+	bullet = bullet_scene.instantiate()
+	bullet.team = self.team
+	bullet.position = global_position
+	bullet.position.y = 1
+	bullet.direction = dir.rotated(Vector3.UP, (TAU * 4) / 3)
+	bullet.lifetime = 4
+	bullet.movement = base_bullet.movement_types.LOOPY
+	bullet.element_type = element
+	bullet.homing = true
+	bullet.radius = 2.0
 	bullet_parent.add_child(bullet)
